97481d247554c4c4f9626b88b3bf214efe07632f
luki <lukimartinezbayon@gmail.com>
2019-05-14 14:23:59 +0200

    sistema de login completado

--- controlador/Usuario_controlador.php
+++ controlador/Usuario_controlador.php
@@ -1,35 +1,49 @@
 <?php
-    
-    class Usuario_controlador{
-        private $usuario_modelo;
-        private $usuario_vista; 
-        
-        function Usuario_controlador(){
-            require_once(dirname(__FILE__).'\..\modelo\Usuario_modelo.php');
-            require_once(dirname(__FILE__).'\..\vista\Usuario_vista.php');
-            
-            $this->usuario_vista= new Usuario_vista();
-        }
-        
-        function verificarUsuario(){
+
+class Usuario_controlador {
+
+    private $usuario_modelo;
+    private $usuario_vista;
+
+    function Usuario_controlador() {
+        require_once(dirname(__FILE__) . '\..\modelo\Usuario_modelo.php');
+        require_once(dirname(__FILE__) . '\..\vista\Usuario_vista.php');
+
+        $this->usuario_vista = new Usuario_vista();
+    }
+
+    function verificarUsuario() {
+        try {
             $this->usuario_vista->mostrarLogin();
-            if($_SESSION){
-                
+            if (isset($_REQUEST['nombreLogin']) && isset($_REQUEST['passwordLogin'])) {
+                $email = htmlentities(addslashes($_REQUEST['nombreLogin']));
+                $password = htmlentities(addslashes($_REQUEST['passwordLogin']));
+                $this->usuario_modelo= new Usuario_modelo($email, $password);
+                if($this->usuario_modelo->verificarUsuario()){
+                    header("Location:contenido.php");
+                } else{
+                    echo 'Usuario no registrado';
+                }
             }
-        }
-        
-        function registrarUsuario(){
-            $this->usuario_vista->mostrarRegistro();
-            if(isset($_REQUEST['emailRegistro']) && isset($_REQUEST['passwordRegistro']) && isset($_REQUEST['nombreRegistro']) && isset($_REQUEST['fechaRegistro']) && isset($_REQUEST['sexoRegistro']) && $_FILES['fotoRegistro']['error'] === UPLOAD_ERR_OK){
-                $contrasenia= $_REQUEST["passwordRegistro"];
-                $pass_cifrado= password_hash($contrasenia, PASSWORD_DEFAULT);
-                $nombreFoto=$_FILES['fotoRegistro']['name'];
-                $carpetaImagen=$_SERVER['DOCUMENT_ROOT'].'/blog/imagen/';
-                move_uploaded_file($_FILES['fotoRegistro']['tmp_name'], $carpetaImagen.$nombreFoto);
-                $this->usuario_modelo= new Usuario_modelo($_REQUEST['emailRegistro'], $pass_cifrado, $_REQUEST['nombreRegistro'], $_REQUEST['fechaRegistro'], $_REQUEST['sexoRegistro'], $nombreFoto);
-                $this->usuario_modelo->insertarUsuario();
-            } else {
-                echo 'No se ha podido realizar el registro.';
-            }
+        } catch (Exception $ex) {
+            die('Error' . $ex->getMessage());
+            echo 'Línea del error: ' . $ex->getLine();
         }
     }
+
+    function registrarUsuario() {
+        $this->usuario_vista->mostrarRegistro();
+        if (isset($_REQUEST['emailRegistro']) && isset($_REQUEST['passwordRegistro']) && isset($_REQUEST['nombreRegistro']) && isset($_REQUEST['fechaRegistro']) && isset($_REQUEST['sexoRegistro']) && $_FILES['fotoRegistro']['error'] === UPLOAD_ERR_OK) {
+            $contrasenia = $_REQUEST["passwordRegistro"];
+            $pass_cifrado = password_hash($contrasenia, PASSWORD_DEFAULT);
+            $nombreFoto = $_FILES['fotoRegistro']['name'];
+            $carpetaImagen = $_SERVER['DOCUMENT_ROOT'] . '/blog/imagen/';
+            move_uploaded_file($_FILES['fotoRegistro']['tmp_name'], $carpetaImagen . $nombreFoto);
+            $this->usuario_modelo = new Usuario_modelo($_REQUEST['emailRegistro'], $pass_cifrado, $_REQUEST['nombreRegistro'], $_REQUEST['fechaRegistro'], $_REQUEST['sexoRegistro'], $nombreFoto);
+            $this->usuario_modelo->insertarUsuario();
+        } else {
+            echo 'No se ha podido realizar el registro.';
+        }
+    }
+
+}
--- modelo/Usuario_modelo.php
+++ modelo/Usuario_modelo.php
@@ -1,6 +1,7 @@
 <?php
 
-class Usuario_modelo{
+class Usuario_modelo {
+
     private $db;
     private $email;
     private $password;
@@ -8,29 +9,61 @@ class Usuario_modelo{
     private $fechaNacimiento;
     private $sexo;
     private $foto;
-    
-    public function __construct($email, $password, $nombre, $fechaNacimiento, $sexo, $foto) {
-        require_once(dirname(__FILE__).'\Conexion.php');
-        $conectar=new Conexion();
-        $this->db=$conectar->ObtenerConexion();
-        $this->email=$email;
-        $this->password=$password;
-        $this->nombre=$nombre;
-        $this->fechaNacimiento=$fechaNacimiento;
-        $this->sexo=$sexo;
-        $this->foto=$foto;
+
+    public function __construct() {
+        $params = func_get_args();
+        $num_params = func_num_args();
+        $funcion_constructor = '__construct' . $num_params;
+        if (method_exists($this, $funcion_constructor)) {
+            call_user_func_array(array($this, $funcion_constructor), $params);
+        }
     }
-    
+    public function __construct2($email, $password) {
+        require_once(dirname(__FILE__) . '\Conexion.php');
+        $conectar = new Conexion();
+        $this->db = $conectar->ObtenerConexion();
+        $this->email = $email;
+        $this->password = $password;
+    }
+    public function __construct6($email, $password, $nombre, $fechaNacimiento, $sexo, $foto) {
+        require_once(dirname(__FILE__) . '\Conexion.php');
+        $conectar = new Conexion();
+        $this->db = $conectar->ObtenerConexion();
+        $this->email = $email;
+        $this->password = $password;
+        $this->nombre = $nombre;
+        $this->fechaNacimiento = $fechaNacimiento;
+        $this->sexo = $sexo;
+        $this->foto = $foto;
+    }
+
     public function insertarUsuario() {
-        try{
-            $sql="INSERT INTO usuarios (email, pass, fechanac, nombre, sexo, foto) VALUES (:email, :pass, :fechanac, :nombre, :sexo, :foto)";
+        try {
+            $sql = "INSERT INTO usuarios (email, pass, fechanac, nombre, sexo, foto) VALUES (:email, :pass, :fechanac, :nombre, :sexo, :foto)";
             $insertar = $this->db->prepare($sql);
-            $insertar->execute(array(":email"=>$this->email, ":pass"=>$this->password, ":nombre"=>$this->nombre, ":fechanac"=>$this->fechaNacimiento, ":sexo"=>$this->sexo, ":foto"=>$this->foto));
+            $insertar->execute(array(":email" => $this->email, ":pass" => $this->password, ":nombre" => $this->nombre, ":fechanac" => $this->fechaNacimiento, ":sexo" => $this->sexo, ":foto" => $this->foto));
         } catch (Exception $ex) {
             die('Error' . $ex->getMessage());
-            echo 'Línea del error: '.$ex->getLine();
+            echo 'Línea del error: ' . $ex->getLine();
         }
-        
     }
-    
+
+    public function verificarUsuario() {
+        try {
+            $existeUsuario = false;
+            $sqlConsultarUsuario = "SELECT * FROM usuarios WHERE email= :email";
+            $resultado = $this->db->prepare($sqlConsultarUsuario);
+            $resultado->execute(array(":email" => $this->email));
+            while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                if (password_verify($this->password, $registro['pass'])) {
+                    $existeUsuario = true;
+                }
+            }
+            return $existeUsuario;
+        } catch (Exception $ex) {
+            die('Error' . $ex->getMessage());
+            echo 'Línea del error: ' . $ex->getLine();
+        }
+    }
+
 }
